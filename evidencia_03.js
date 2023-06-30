34c1b959ddba9fbab9d69b7ff899699d8443c4a8
Brillant-Minds-Grupo-5 <mosanz02@gmail.com>
2023-06-30 16:33:10 -0500

    V0.2

--- evidencia_03.js
+++ evidencia_03.js
@@ -1,43 +1,22 @@
-/*Realizar un algoritmo que muestre por pantalla la tabla de multiplicar decreciente de cualquier número,
-ingresado entre el 1 y el 10.*/
 
-function mostrarTabla(){
-    //obtener el numero ingresado por el usuario
-    var numero= parseInt(document.getElementById("numero").value);
 
-    //validar que el numero este dentro del rango valido
+function determinarAñoBisiesto() {
+    var año = document.getElementById("inputYear").value;
+    var esBisiesto = false;
 
-    if(isNaN(numero) || numero < 1 || numero > 10) {
-        alert("Por favor, ingrese un número válido entre 1 y 10.");
-        return;
+    if (año % 4 === 0) {
+        if (año % 100 === 0) {
+            if (año % 400 === 0) {
+                esBisiesto = true;
+            }
+        } else {
+            esBisiesto = true;
+        }
     }
 
-    //obtener el elemento de la tabla
-    var tabla = document.getElementById("tabla-multiplicar");
-
-    //limpiar la tabla 
-    tabla.innerHTML="";
-     //generar la tabla de multiplicar decreciente
-     for (var i=10; i>= 1; i--) {
-        var resultado = numero  *  i;
-
-        var fila = document.createElement("tr");
-        var celdaNumero = document.createElement("td");
-        var celdaMultiplicador = document.createElement("td");
-        var celdaResultado = document.createElement("td");
-
-        celdaNumero.textContent= numero;
-        celdaMultiplicador.textContent= i;
-        celdaResultado.textContent=resultado;
-
-        fila.appendChild(celdaNumero);
-        fila.appendChild(celdaMultiplicador);
-        fila.appendChild(celdaResultado);
-
-
-        tabla.appendChild(fila);
-     }
+    if (esBisiesto) {
+        document.getElementById("resultado").textContent = año + " es un año bisiesto.";
+    } else {
+        document.getElementById("resultado").textContent = año + " no es un año bisiesto.";
+    }
 }
-
-
-
